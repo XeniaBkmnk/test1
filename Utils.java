c2077d55e7820c7901ceb123b2f28233cbdd7e02
Kseniya-Bakumenko <xenia.bkmnk@gmail.com>
2023-10-10 23:08:28 +0300

    Merge remote-tracking branch 'origin/main'
    # add hello

--- Utils.java
+++ Utils.java
@@ -1,20 +1,5 @@
-public
-<<<<<<<<< Temporary merge branch 1
-    class Utils {
-    public static void check(int x, int y, int z){
-        if(x>0 && y>0 && z>0){
-            System.out.println("excellent");
-        }else if((x>0 && y>0)||(x>0 && z>0)|| (y>0 && z>0)){
-            System.out.println("good");
-        }else if(x>0 || y>0 || z>0){
-            System.out.println("fine");
-        }else System.out.println("bad");
-=========
-class Utils {
+public class Utils {
     public static void main(String[] args) {
-        System.out.println("");
->>>>>>>>> Temporary merge branch 2
+        System.out.println("hello");
     }
-
-
 }
